f1dad8a57471cc85bd7c2a99da30ae63412b048e
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-09-12 00:32:53 +0530

    Implement seller dashboard

--- app/store/layout.jsx
+++ app/store/layout.jsx
@@ -1,5 +1,5 @@
 import StoreLayout from "@/components/store/StoreLayout";
-
+import {SignedIn, SignIn, SignedOut} from "@clerk/nextjs"
 export const metadata = {
     title: "GoCart. - Store Dashboard",
     description: "GoCart. - Store Dashboard",
@@ -9,9 +9,16 @@ export default function RootAdminLayout({ children }) {
 
     return (
         <>
+        <SignedIn>
             <StoreLayout>
                 {children}
             </StoreLayout>
+            </SignedIn>
+            <SignedOut>
+                <div className="min-h-screen flex items-center justify-center">
+                <SignIn fallbackRedirectUrl="/store" routing="hash" />
+                </div>
+            </SignedOut>
         </>
     );
 }
