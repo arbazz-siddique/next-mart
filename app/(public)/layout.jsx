407abc58b3d431eb34f304732720d86ab22dbff2
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-09-13 10:40:42 +0530

    Implement functionality of address

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "@/lib/features/product/productSlice";
 import { useAuth, useUser } from "@clerk/nextjs";
 import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
+import { fetchAddress } from "@/lib/features/address/addressSlice";
 
 export default function PublicLayout({ children }) {
     const dispatch = useDispatch()
@@ -21,6 +22,7 @@ export default function PublicLayout({ children }) {
     useEffect(()=>{
         if(user){
             dispatch(fetchCart({getToken}))
+            dispatch(fetchAddress({getToken}))
         }
     },[user])
 
